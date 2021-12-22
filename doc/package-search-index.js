7d31e0e72c3288548486afff358998af1d87afa5
Joel Jerez <jjerezalo@gmail.com>
2021-12-22 09:15:14 -0300

    add javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jereztech.restcountries"},{"l":"com.jereztech.restcountries.support"},{"l":"com.jereztech.restcountries.v1.data"},{"l":"com.jereztech.restcountries.v1.services"},{"l":"com.jereztech.restcountries.v1.ws"}];updateSearchResults();
